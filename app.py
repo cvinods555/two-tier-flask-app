4f119a44e96ab6629aba8f84a5afbdde3fed7f86
Vinod Sharma <30870963+cvinods555@users.noreply.github.com>
2024-07-25 17:22:07 +0530

    updated app.py
    Debugging Feedback and Retry Mechanism Added

--- app.py
+++ app.py
@@ -4,12 +4,6 @@ from flask import Flask, render_template, request, jsonify
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-
-app.config['MYSQL_HOST'] = 'mysql'
-app.config['MYSQL_USER'] = 'admin'
-app.config['MYSQL_PASSWORD'] = 'admin'
-app.config['MYSQL_DB'] = 'myDb'
-
 mysql = MySQL(app)
 
 def init_db():
